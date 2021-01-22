5159575bcb899e43857b7e29cd2f400002d72c52
Mateo León Camacho <amateoleonc@outlook.com>
2021-01-21 23:26:16 -0500

    Solution for problem 2

--- Problems1-10.cpp
+++ Problems1-10.cpp
@@ -14,14 +14,23 @@ int multiplesof3and5 (int lim) { //recursive function to determine if a given nu
     return multiplesof3and5(lim - 1); //otherwise
 }
 
-long evenFib (int lim) {
+/*
+Fibonacci numbers are built adding the previous two term in the series, with 0 and 1 as seeds.
+F(n) = F(n - 1) + F(n - 2) --> F(2) = F(1) + F(0) = 1 + 0 = 1
+F(3) = F(2) + F(1) = 1 + 1 = 2 {3, 5, 8, 13, 21, 34, ...}
+
+Even Fibonacci numbers follow the ecuation EvF(n) = 4 * EvF(n - 1) + EvF(n - 2). Seeds are 0 and 2.
+*/
+
+
+long evenFib (int lim) { //Returns the sum of all even Fibonacci numbers below given limit
     long res, i1, i2;
     i1 = 0;
     i2 = 2;
     while (i2 < lim) {
-        res += i2;
-        i2 = 4 * i2 + i1;
-        i1 = (i2 - i1) / 4;
+        res += i2; //res variable stores the partial sum of even numbers
+        i2 = 4 * i2 + i1; //Get new Fib number
+        i1 = (i2 - i1) / 4; //Get previous i2 state and stores it
     }
     return res;
 }
